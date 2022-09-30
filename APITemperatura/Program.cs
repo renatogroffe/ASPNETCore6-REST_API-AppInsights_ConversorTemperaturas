de590c2a15b364ec7edc5236b7e5e49977804e95
Renato Groffe <renato.groffe@yahoo.com.br>
2022-09-30 17:32:01 -0300

    .NET 6 + ASP.NET Core + Azure Application Insights + API REST para conversão de temperaturas (incluindo simulação de falhas)
    .NET 6 + ASP.NET Core + Azure Application Insights + API REST para conversão de temperaturas (incluindo simulação de falhas)

---
+++ APITemperatura/Program.cs
@@ -0,0 +1,45 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1",
+        new OpenApiInfo
+        {
+            Title = nameof(APITemperatura),
+            Description = "API REST para conversão de temperaturas", 
+            Version = "v1",
+            Contact = new OpenApiContact()
+            {
+                Name = "Renato Groffe",
+                Url = new Uri("https://github.com/renatogroffe"),
+            },
+            License = new OpenApiLicense()
+            {
+                Name = "MIT",
+                Url = new Uri("http://opensource.org/licenses/MIT"),
+            }
+        });
+});
+
+if (!String.IsNullOrWhiteSpace(builder.Configuration.GetConnectionString("ApplicationInsights")))
+    builder.Services.AddApplicationInsightsTelemetry(options =>
+        {
+            options.ConnectionString = builder.Configuration.GetConnectionString("ApplicationInsights");
+        });
+        
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
